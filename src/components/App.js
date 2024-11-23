3b3db45a21c90bd608751bb047f5ee625c76709a
sachin7con <sachin7con@gmail.com>
2024-11-23 15:57:58 +0530

    removed hardcoding values

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 //SGN
-import React, { useState } from "react";
-import {BrowserRouter as Router, Routes, Route, Link, Navigate, Outlet} from "react-router-dom";
-import "./../styles/App.css";
+import React from "react";
+import {BrowserRouter as Router, Routes, Route, Link, Outlet} from "react-router-dom";
 
-const Home = () => <p>Home</p>;
-const Shirt = () => <p>Shirt</p>;
-const Trouser = () => <p>Trouser</p>;
-const Jewellery = () => <p>Jewellery</p>;
+const categories = ["Women", "Men", "Kids"];
+const items = {
+  Women: ["Shirt", "Trouser", "Jewellery"],
+  Men: ["Shirt", "Trouser", "Watch"],
+  Kids: ["Toys", "Books"]
+};
 
+const Item = ({name}) => <p>{name}</p>;
+
+const Category = ({name}) => {
+  return(
+    <div>
+    <p>{name} Items:</p>
+    <ul>
+      {items[name].map(item =>
+        <li key={item}>
+          <Link to={item}>{item}</Link>
+        </li>
+      )}
+    </ul>
+    <Outlet />
+    </div>
+  )
+}
 
 const Layout = () => {
   return(
@@ -17,9 +35,11 @@ const Layout = () => {
             <li>
               <Link to="/">Home</Link>
             </li>
-            <li>
-              <Link to="/women">Women</Link>
-            </li>
+            {categories.map(category =>
+              <li key={category}>
+                <Link to={category}>{category}</Link>
+              </li>
+            )}
           </ul>
         </nav>
         <Outlet />
@@ -27,50 +47,19 @@ const Layout = () => {
   )
 }
 
-const Women= () => {
-  return(
-    <div>
-    <p>Women Items:</p>
-    <ul>
-        <li>
-          <Link to="grooming">Grooming</Link>
-        </li>
-        <li>
-          <Link to="shirt">Shirt</Link>
-        </li>
-        <li>
-          <Link to="trouser">Trouser</Link>
-        </li>
-        <li>
-          <Link to="jewellery">Jewellery</Link>
-        </li>
-      </ul>
-      {/* Nested Route Content */}
-      <Outlet />
-
-    </div>
-  )
-}
-
-const Grooming = () => <p>Grooming</p>;
 const App = () => {
   return (
     <div>
-      {/* ONS Do not remove the main div */}
       <Router>
-
-
         <Routes>
           <Route path="/" element={<Layout />} >
-          <Route path="/" element={<Home />} />
-          {/* Parent Route for Women */}
-          <Route path="/women" element={<Women />}>
-            {/* Nested Route for Grooming */}
-            <Route path="grooming" element={<Grooming />} />
-            <Route path="shirt" element={<Shirt />} />
-            <Route path="trouser" element={<Trouser />} />
-            <Route path="jewellery" element={<Jewellery />} />
-          </Route>
+            {categories.map(category =>
+              <Route key={category} path={category} element={<Category name={category} />}>
+                {items[category].map(item =>
+                  <Route key={item} path={item} element={<Item name={item} />} />
+                )}
+              </Route>
+            )}
           </Route>
         </Routes>
       </Router>
